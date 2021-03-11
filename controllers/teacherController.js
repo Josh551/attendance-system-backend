0a0065d80c0bad0811385c79a3bf29729556dcac
G Sriram <sgcuber24@gmail.com>
2021-03-11 16:45:44 +0530

    Added teacher protection

--- controllers/teacherController.js
+++ controllers/teacherController.js
@@ -5,106 +5,96 @@ import Teacher from '../models/teacherModel.js';
 // @route   POST /api/teacher/login
 // @access  Public
 const authTeacher = asyncHandler(async (req, res) => {
-  const { email, password } = req.body;
+	const { email, password } = req.body;
 
-  const teacher = await Teacher.findOne({ email });
+	const teacher = await Teacher.findOne({ email });
 
-  if (teacher) {
-    res.json({
-      token: generateToken(teacher._id),
-    });
-  } else {
-    res.status(401);
-    throw new Error('Invalid email or password');
-  }
+	if (teacher) {
+		res.json({
+			token: generateToken(teacher._id)
+		});
+	} else {
+		res.status(401);
+		throw new Error('Invalid email or password');
+	}
 });
 
 // @route   POST /api/teacher/register
 // @access  Private/Admin
 const registerTeacher = asyncHandler(async (req, res) => {
-  const { empId, email, fullName, password } = req.body;
+	const { empId, email, fullName, password } = req.body;
 
-  const teacherExists = await Teacher.findOne({ email });
+	const teacherExists = await Teacher.findOne({ email });
 
-  if (teacherExists) {
-    res.status(400);
-    throw new Error('User already exists');
-  }
+	if (teacherExists) {
+		res.status(400);
+		throw new Error('User already exists');
+	}
 
-  const teacher = await Teacher.create({
-    empId,
-    email,
-    fullName,
-    password,
-    addedBy: req.admin._id,
-    token: generateToken(teacher._id),
-  });
-  const createdTeacher = await teacher.save();
-  if (createdTeacher) {
-    res.status(201).json({
-      teacher,
-    });
-  } else {
-    res.status(400);
-    throw new Error('Invalid user data');
-  }
+	const teacher = await Teacher.create({
+		empId,
+		email,
+		fullName,
+		password,
+		addedBy: req.admin._id,
+		token: generateToken(teacher._id)
+	});
+	const createdTeacher = await teacher.save();
+	if (createdTeacher) {
+		res.status(201).json({
+			teacher
+		});
+	} else {
+		res.status(400);
+		throw new Error('Invalid user data');
+	}
 });
 
 // @route   GET /api/teacher/:id
 // @access  Private/Admin
 const getTeachers = asyncHandler(async (req, res) => {
-  const teachers = await Teacher.find({}).populate('admin', 'empId fullname');
-  res.json(teachers);
+	const teachers = await Teacher.find({}).populate('admin', 'empId fullname');
+	res.json(teachers);
 });
 
 // @route   Get /api/teacher/profile
 // @access  Private
 const getTeacherDetails = asyncHandler(async (req, res) => {
-  const teacher = await Teacher.findById(req.teacher._id);
-  if (teacher) {
-    res.json(teacher);
-  } else {
-    res.status(404);
-    throw new Error('Teacher not Found');
-  }
+	const teacher = await Teacher.findById(req.teacher._id).select('-password');
+	if (teacher) {
+		res.json(teacher);
+	} else {
+		res.status(404);
+		throw new Error('Teacher not Found');
+	}
 });
 // @route   GET /api/teacher/:id
 // @access  Private
 const getTeacherProfile = asyncHandler(async (req, res) => {
-  const teacher = await Teacher.findById(req.params.id).populate(
-    'admin',
-    'empId fullName'
-  );
+	const teacher = await Teacher.findById(req.params.id).populate('admin', 'empId fullName');
 
-  if (teacher) {
-    res.json({
-      teacher,
-    });
-  } else {
-    res.status(404);
-    throw new Error('Teacher not found');
-  }
+	if (teacher) {
+		res.json({
+			teacher
+		});
+	} else {
+		res.status(404);
+		throw new Error('Teacher not found');
+	}
 });
 
 // @route   DELETE /api/teacher/:id
 // @access  Private
 const deletedTeacher = asyncHandler(async (req, res) => {
-  const teachers = await Teacher.findById(req.params.id);
+	const teachers = await Teacher.findById(req.params.id);
 
-  if (teachers) {
-    await teacher.remove();
-    res.json({ message: 'Teacher removed by admin' });
-  } else {
-    res.status(404);
-    throw new Error('Teacher not found');
-  }
+	if (teachers) {
+		await teacher.remove();
+		res.json({ message: 'Teacher removed by admin' });
+	} else {
+		res.status(404);
+		throw new Error('Teacher not found');
+	}
 });
 
-export {
-  authTeacher,
-  registerTeacher,
-  getTeachers,
-  getTeacherProfile,
-  getTeacherDetails,
-  deletedTeacher,
-};
+export { authTeacher, registerTeacher, getTeachers, getTeacherProfile, getTeacherDetails, deletedTeacher };
--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -4,97 +4,88 @@ import Admin from '../models/adminModel.js';
 import Teacher from '../models/teacherModel.js';
 
 const protect = asyncHandler(async (req, res, next) => {
-  let token;
+	let token;
 
-  if (
-    req.headers.authorization &&
-    req.headers.authorization.startsWith('Bearer')
-  ) {
-    try {
-      token = req.headers.authorization.split(' ')[1];
+	if (req.headers.authorization && req.headers.authorization.startsWith('Bearer')) {
+		try {
+			token = req.headers.authorization.split(' ')[1];
 
-      const decoded = jwt.verify(token, 'secretOrKey');
+			const decoded = jwt.verify(token, 'secretOrKey');
 
-      let userData =
-        (await Admin.findById(decoded.id).select('-password')) ||
-        (await Teacher.findById(decoded.id).select('-password'));
-      if (!userData) {
-        res.status(401);
-        throw new Error('Not authorized, Not Admin');
-      }
-      next();
-    } catch (error) {
-      console.error(error);
-      res.status(401);
-      throw new Error('Not authorized, token failed');
-    }
-  }
+			let userData =
+				(await Admin.findById(decoded.id).select('-password')) ||
+				(await Teacher.findById(decoded.id).select('-password'));
+			if (!userData) {
+				res.status(401);
+				throw new Error('Not authorized, Not Admin');
+			}
+			next();
+		} catch (error) {
+			console.error(error);
+			res.status(401);
+			throw new Error('Not authorized, token failed');
+		}
+	}
 
-  if (!token) {
-    res.status(401);
-    throw new Error('Not authorized, no token');
-  }
+	if (!token) {
+		res.status(401);
+		throw new Error('Not authorized, no token');
+	}
 });
 
 const admin = asyncHandler(async (req, res, next) => {
-  let token;
+	let token;
 
-  if (
-    req.headers.authorization &&
-    req.headers.authorization.startsWith('Bearer')
-  ) {
-    try {
-      token = req.headers.authorization.split(' ')[1];
+	if (req.headers.authorization && req.headers.authorization.startsWith('Bearer')) {
+		try {
+			token = req.headers.authorization.split(' ')[1];
 
-      const decoded = jwt.verify(token, 'secretOrKey');
+			const decoded = jwt.verify(token, 'secretOrKey');
 
-      let adminData = await Admin.findById(decoded.id).select('-password');
-      if (!adminData) {
-        res.status(401);
-        throw new Error('Not authorized, Not Admin');
-      }
-      next();
-    } catch (error) {
-      console.error(error);
-      res.status(401);
-      throw new Error(error.message);
-    }
-  }
+			req.admin = await Admin.findById(decoded.id).select('-password');
+			if (!req.admin) {
+				res.status(401);
+				throw new Error('Not authorized, Not Admin');
+			}
+			next();
+		} catch (error) {
+			console.error(error);
+			res.status(401);
+			throw new Error(error.message);
+		}
+	}
 
-  if (!token) {
-    res.status(401);
-    throw new Error('Not authorized, no token');
-  }
+	if (!token) {
+		res.status(401);
+		throw new Error('Not authorized, no token');
+	}
 });
 
 const teacher = asyncHandler(async (req, res, next) => {
-  let token;
+	let token;
 
-  if (
-    req.headers.authorization &&
-    req.headers.authorization.startsWith('Bearer')
-  ) {
-    try {
-      token = req.headers.authorization.split(' ')[1];
+	if (req.headers.authorization && req.headers.authorization.startsWith('Bearer')) {
+		try {
+			token = req.headers.authorization.split(' ')[1];
 
-      const decoded = jwt.verify(token, 'secretOrKey');
+			const decoded = jwt.verify(token, 'secretOrKey');
 
-      let teacherData = await Teacher.findById(decoded.id).select('-password');
-      if (!teacherData) {
-        res.status(401);
-        throw new Error('Not authorized, Not teacher');
-      }
-      next();
-    } catch (error) {
-      console.error(error);
-      res.status(401);
-      throw new Error(error.message);
-    }
-  }
+			req.teacher = await Teacher.findById(decoded.id).select('-password');
+			if (!req.teacher) {
+				res.status(401);
+				throw new Error('Not authorized, Not teacher');
+			}
+			next();
+		} catch (error) {
+			console.error(error);
+			res.status(401);
+			throw new Error(error.message);
+		}
+	}
 
-  if (!token) {
-    res.status(401);
-    throw new Error('Not authorized, no token');
-  }
+	if (!token) {
+		res.status(401);
+		throw new Error('Not authorized, no token');
+	}
 });
 export { protect, admin, teacher };
