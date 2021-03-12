83783784118ed20a73f07a8a892e9146f1385698
Cool Stats <joshuaniteesh551@gmail.com>
2021-03-12 07:42:22 +0530

    Hosted backend

--- server.js
+++ server.js
@@ -28,17 +28,9 @@ app.use('/api/teacher', teacherRoutes);
 app.use('/api/student', studentRoutes);
 app.use('/api/attendance', attendanceRoutes);
 
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '/frontend/build')));
-
-  app.get('*', (req, res) =>
-    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
-  );
-} else {
-  app.get('/', (req, res) => {
-    res.send('API is running....');
-  });
-}
+app.get('/', function (req, res) {
+  res.sendFile(__dirname + '/index.html');
+});
 app.use(notFound);
 app.use(errorHandler);
 
