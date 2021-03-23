b8e4edd0d05874dc6d477c072b2d025e192043a0
sgcuber24 <sgcuber24@gmail.com>
2021-03-23 12:42:39 +0530

    Added update attendance

--- controllers/attendanceController.js
+++ controllers/attendanceController.js
@@ -21,10 +21,31 @@ const enterAttendance = asyncHandler(async (req, res) => {
 	}
 });
 
+// @route   PUT /api/attendance/register/:id
+// @access  Private/Admin
+const updateAttendance = asyncHandler(async (req, res) => {
+	const { absent } = req.body;
+
+	const attendance = await Attendance.findById(req.params.id);
+	if (attendance) {
+		attendance.absent = absent;
+		const createdAttendance = await attendance.save();
+		if (createdAttendance) {
+			res.status(201).json({
+				createdAttendance
+			});
+		} else {
+			res.status(400);
+			throw new Error('Invalid attendance data');
+		}
+	} else {
+		throw new Error('Attendance Document not found');
+	}
+});
 // @route   GET /api/attendance/byClass/:class_id
 // @access  Private
 const getAttendanceByClass = asyncHandler(async (req, res) => {
-	const attendance = await Attendance.find({ classes: req.params.class_id });
+	const attendance = await Attendance.find({ classId: req.params.class_id });
 
 	if (attendance) {
 		res.json({
@@ -40,7 +61,7 @@ const getAttendanceByClass = asyncHandler(async (req, res) => {
 // @access  Private
 const getAttendanceLimited = asyncHandler(async (req, res) => {
 	const attendance = await Attendance.find({
-		classes: req.params.class_id
+		classId: req.params.class_id
 	}).limit(20);
 
 	if (attendance) {
@@ -53,4 +74,4 @@ const getAttendanceLimited = asyncHandler(async (req, res) => {
 	}
 });
 
-export { enterAttendance, getAttendanceByClass, getAttendanceLimited };
+export { enterAttendance, getAttendanceByClass, getAttendanceLimited, updateAttendance };
--- routes/attendanceRoutes.js
+++ routes/attendanceRoutes.js
@@ -1,10 +1,16 @@
 import express from 'express';
 const router = express.Router();
-import { enterAttendance, getAttendanceByClass, getAttendanceLimited } from '../controllers/attendanceController.js';
+import {
+	enterAttendance,
+	getAttendanceByClass,
+	getAttendanceLimited,
+	updateAttendance
+} from '../controllers/attendanceController.js';
 import { protect, admin, teacher } from '../middleware/authMiddleware.js';
 
 router.route('/register').post(protect, admin, enterAttendance);
 router.get('/test', (req, res) => res.json({ msg: 'Users Works' }));
 router.route('/byClass/:class_id').get(protect, teacher, getAttendanceByClass);
 router.route('/byClass/lm/:class_id').get(protect, teacher, getAttendanceLimited);
+router.route('/:id').put(protect, updateAttendance);
 export default router;
